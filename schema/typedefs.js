f820cf1243db9bc5e61c7ab47734c92181c903f3
edlazzari8 <73348450+edlazzari8@users.noreply.github.com>
2021-11-02 18:14:35 +0100

    Update typedefs.js

--- schema/typedefs.js
+++ schema/typedefs.js
@@ -10,5 +10,44 @@ module.exports= gql`
         name: String
         movies: [Movie] @relationship(type: "ACTED_IN", direction: OUT)
     }
+input Libroinput{
+    titolo: String!
+    descrizione: String!
+    autore : String!
+}
+input Autoreinput{
+    nome: String!
+}
 
+type Query {
+    tutti_libri:[Libro] @cypher(
+        statement:"""
+        MATCH (a:Libro) - [:STATO_SCRITTO_DA] -> (b:Autore)
+        return a,b
+    
+    """)
+    autori:[Autore] @cypher(
+        statement:"""
+        MATCH (a:Autore) - [:HA_SCRITTO] -> (b:Libro)
+        return (a),(b)
+    
+    """)
+
+}
+type Mutation {
+    aggiungi_libro(libroinput: Libroinput) :Libro @cypher(
+        statement:"""
+        MERGE (a:Libro {titolo: $libroinput.titolo, descrizione: $libroinput.descrizione})
+        MERGE (b:Autore {nome: $libroinput.autore})
+        MERGE (a) <- [:HA_SCRITTO] - (b)
+        MERGE (b) <- [:STATO_SCRITTO_DA] - (a)
+        return a,b
+    """)
+    aggiungi_autore(autoreinput: Autoreinput) : Autore @cypher(
+        statement:"""
+        MERGE (b:Autore {nome: $autoreinput.nome})
+        return (b)
+    """)
+    
+}
 `;
